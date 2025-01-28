9ff4f6dae304a3152ebd9bd96d6e07e7220d9c1d
LBPR0 <lbpro10000@gmail.com>
2025-01-28 12:44:36 +0400

    commit

---
+++ day 01/homework/lesson2.py
@@ -0,0 +1,15 @@
+name  = "ლევანი" 
+surname = "ვარდიშვილი" 
+#ეს ორი არის str (string) ტიპის ცვლადი
+age = 14 # ეს არის int (integer) მთელი რიცხვი
+height = 179.5 # ეს არის ტიპის ცვლადი (აწილადი) 
+
+
+#boolean (bool) ტიპის ცვლადი
+
+goes_to_school = True
+has_a_cat = False 
+
+print(name + " " + surname)
+print(name + " " + str(age))
+print("მე" + " " + "ვარ" + " " +  name + " " + surname + " " + "ვარ" + " " + str(age) + "-ის" + " " + "და" + " " + "მე" + " " + "მომონს" + " " + "პროგრამირება")
